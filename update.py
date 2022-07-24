d381c85392b791f9c339265c88adfd70a4157b6b
cvatricks <68840422+cvatricks@users.noreply.github.com>
2022-07-24 17:11:22 +0530

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/anasty17/mirror-leech-telegram-bot"
+    UPSTREAM_REPO = "https://github.com/cvatricks/Mirror-Leech-Bot"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
